915e154f687656542c69bc91e46b7f57fb919b33
Jul-code <up02439@gmail.com>
2021-11-22 13:18:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import controllers.ClientController;
+import models.Client;
+import views.ClientView;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Client model = new Client();
+        ClientView view = new ClientView(model);
+        ClientController controller = new ClientController(model, view);
+        // Запуск программы/приложения
+        controller.runApp();
+    }
+}
---
+++ src/base/Payment.java
@@ -0,0 +1,9 @@
+package base;
+
+public interface Payment {
+
+    double calculatePayment(int quota, double price);
+
+    double calculatePayment(double payment, double discount);
+
+}
