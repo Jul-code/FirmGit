43a2ad9fef1b24537d41ddd4871a6265ac20246e
Jul-code <up02439@gmail.com>
2021-11-23 15:20:42 +0200

    Change Validator for Seller

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -1,14 +1,15 @@
 package utils;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Validator {
     // Валидация ввода имени
-    public static String validateName(Scanner scanner) {
+    public static String validateName(Scanner scanner, String job) {
         String str = scanner.nextLine().trim();
         while (str.isEmpty()) {
-            System.out.print("Пусто! Введите имя клиента: ");
+            System.out.print("Пусто! Введите имя " + job + ": ");//добавлен
+            // параметр "job" в метод, чтобы использовать один
+            // метод для продавца и клиента одновременно
             str = scanner.nextLine().trim();
         }
         return str;
@@ -52,6 +53,46 @@ public class Validator {
             price = scanner.nextDouble();
         }
         return price;
+    }
+
+    // Валидация ввода количества часов
+    public static int validateHoursInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!\n", str);
+            System.out.print("Введите количество часов!: ");
+        }
+        int hours = scanner.nextInt();
+        while (hours <= 0) {
+            System.out.print("Неверное значение! Введите количество часов!: ");
+            while (!scanner.hasNextInt()) {
+                String str = scanner.nextLine().trim();
+                System.out.printf("\"%s\" - не число!\n", str);
+                System.out.print("Введите количество часов!: ");
+            }
+            hours = scanner.nextInt();
+        }
+        return hours;
+    }
+
+    //Валидация ввода часовой ставки
+    public static double validateHourlyRateInput(Scanner scanner) {
+        while (!scanner.hasNextDouble()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!\n", str);
+            System.out.print("Введите часовую ставку!: ");
+        }
+        double hourlyRate = scanner.nextDouble();
+        while (hourlyRate <= 0) {
+            System.out.print("Неверное значение! Введите часовую ставку!: ");
+            while (!scanner.hasNextDouble()) {
+                String str = scanner.nextLine().trim();
+                System.out.printf("\"%s\" - не число!\n", str);
+                System.out.print("Введите часовую ставку!: ");
+            }
+            hourlyRate = scanner.nextDouble();
+        }
+        return hourlyRate;
     }
 }
 
